ec7131f6c428c3755ccbddd07c22262f183b1f56
TaeHwoi Gu <47591363+zeynut@users.noreply.github.com>
2019-10-23 11:48:48 +0900

    antd row col 적용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Menu, Input, Button } from 'antd';
+import { Menu, Input, Button, Row, Col, Card, Avatar, Icon } from 'antd';
 import Link from 'next/link';
+
+const dummy ={
+    id: "",
+    nickname: "zeynut",
+    Post:[],
+    Followings:[],
+    Followers: [],
+
+}
 
 const AppLayout = ({children}) => {
     return (
@@ -13,8 +22,29 @@ const AppLayout = ({children}) => {
                 <Input.Search enterButton style={{ verticalAlign : "middle"}} />
             </Menu.Item>
         </Menu>
-        <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-            {children}
+        <Row>
+            <Col xs={24} md={8} >
+                <Card 
+                    style={{ width: 320 }}
+                    actions={[<div><Icon type="aliwangwang" key="twit"/>포스트<br/>{dummy.Post.length}</div>,
+                             <div><Icon type="usergroup-add" key="following"/>팔로잉<br/>{dummy.Followings.length}</div>,
+                             <div><Icon type="team" key="followers"/>팔로워<br/>{dummy.Followers.length}</div>]}>
+                    <Card.Meta 
+                    avatar={<Avatar>{dummy.nickname[0]}</Avatar>} 
+                    title={dummy.nickname}
+                    description="이것은 디스크립션입니다." />
+                </Card>
+                <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+            </Col>
+            <Col xs={24} md={8} >
+                {children}
+            </Col>
+            <Col xs={24} md={8} >
+                세번째 컬럼
+            </Col>
+        </Row>
+
+            
         </div>
     );
 }
