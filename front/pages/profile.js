095a2dc6b3c7f6a85c60a5e3f5289653b8feee53
TaeHwoi Gu <47591363+zeynut@users.noreply.github.com>
2019-10-23 15:38:22 +0900

    profile화면레이아웃

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,19 +1,56 @@
 import React from 'react';
+import { Input, Button, Form, List, Card, Icon } from 'antd';
 import PropTypes from 'prop-types';
 
 const Profile = () => {
     return (
         <>
             <div>
-                프로필
+            <Form style={{ marginTop: "20px" , border: " 1px solid #d9d9d9", padding: "15px"}}>
+                    <Input addonBefore="닉네임"></Input>
+                    <Button type="primary">수정</Button>
+            </Form>
+            <List
+                style={{ marginBottom: "20px"}}
+                grid={{ gutter:4, xs: 2, md: 3}}
+                size="small"
+                header={<div>팔로잉 목록</div>}
+                loadMore={<Button style={{ width: "100%"}}>더 보기</Button>}
+                bordered
+                dataSource={["제이넛", "ekhub" , "다람쥐"]}
+                renderItem={
+                    item => (
+                        <List.Item style={{ marginTop : "20px"}}>
+                            <Card actions={[ <Icon key="stop" type="stop"/>]}>
+                                <Card.Meta description={item} />
+                            </Card>
+                        </List.Item>
+                    )}
+                 />            
+            <List
+                style={{ marginBottom: "20px"}}
+                grid={{ gutter:4, xs: 2, md: 3}}
+                size="small"
+                header={<div>팔로워 목록</div>}
+                loadMore={<Button style={{ width: "100%"}}>더 보기</Button>}
+                bordered
+                dataSource={["제이넛", "ekhub" , "다람쥐"]}
+                renderItem={
+                    item => (
+                        <List.Item style={{ marginTop : "20px"}}>
+                            <Card actions={[ <Icon key="stop" type="stop"/>]}>
+                                <Card.Meta description={item} />
+                            </Card>
+                        </List.Item>
+                    )}
+                 />            
             </div>
-            
         </>
     );
 }
 
 Profile.proptypes = {
-    
+
 }
 
 export default Profile;
