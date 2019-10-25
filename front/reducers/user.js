42035de27c5cb21d896ef0a26ea54eba082bb584
TaeHwoi Gu <47591363+zeynut@users.noreply.github.com>
2019-10-25 17:11:46 +0900

    REDUX-SAGA 세팅2

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -17,6 +17,19 @@ export const SIGN_UP = 'SIGN_UP';
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
 
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+
 export const loginAction = {
     type: LOG_IN,
  }
@@ -30,21 +43,21 @@ export const signupAction = (data) => {
 
 const reducer = ( state = initialState , action ) => {
     switch (action.type) {
-        case LOG_IN : {
+        case LOG_IN_REQUEST : {
             return {
                 ...state,
                 isLoggedIn: true,
                 user: dummyUser,
             }
         }
-        case LOG_OUT : {
+        case LOG_OUT_REQUEST : {
             return {
                 ...state,
                 isLoggedIn: false,
                 user: null,
             }
         }
-        case SIGN_UP: {
+        case SIGN_UP_REQUEST: {
             return {
                 ...state,
                 signUpData: action.data,
