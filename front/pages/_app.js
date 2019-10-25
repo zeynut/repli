2438802e09bcd15db541a845c36dafbdaa34134f
TaeHwoi Gu <47591363+zeynut@users.noreply.github.com>
2019-10-25 17:11:09 +0900

    REDUX-SAGA 세팅

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import { createStore , compose , applyMiddleware } from 'redux';
 import withRedux from 'next-redux-wrapper'
 import reducer from '../reducers';
+import createSagaMiddleware from '@redux-saga/core';
+import rootSaga from '../sagas';
 
 
 const Repli = ({Component , store}) => {
@@ -29,10 +31,13 @@ Repli.propTypes = {
 }
 
 export default withRedux( (initialState , options ) => {
-    const middlewares = [];
-    const enhancer = compose(
-        applyMiddleware(...middlewares),
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware];
+    const enhancer = process.env.NODE_ENV ==='product' ?
+        compose(applyMiddleware(...middlewares))
+     : compose(applyMiddleware(...middlewares),
         typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,);
     const store = createStore( reducer, initialState, enhancer);
+    sagaMiddleware.run(rootSaga);
     return store;
 })(Repli);
---
+++ front/sagas/user.js
@@ -0,0 +1,30 @@
+import {all , call, fork, takeLatest } from 'redux-saga/effects';
+import {LOG_IN} from '../reducers/user';
+
+function loginAPI() {
+
+}
+
+function* login(){
+    try{
+            yield call(loginAPI);
+            yield put({
+            type: LOG_IN_SUCCESS
+           })
+    }catch(e){
+            console.error(e);
+            yield put( {
+               type: LOG_IN_FAILURE
+             })
+    }
+}
+
+function* watchLogin(){
+    yield takeLatest(LOG_IN, login);
+}
+
+export default function* userSaga() {
+    yield all([
+        fork(watchLogin),
+    ]);
+}
